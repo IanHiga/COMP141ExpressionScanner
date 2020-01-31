f66d5bac7e2bcc3545e05b2f0b0efb98be6ed67d
Ian Higa <ian.higa@sbcglobal.net>
2020-01-31 09:47:34 -0800

    Added scanner for command line input

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -1,7 +1,19 @@
+import java.io.File;
+import java.util.Scanner;
 
 public class MainApplication {
+	public static final String DIRECTORY = "null"; //TODO add file directory
+	private File input, output;
+	
 	public static void main(String[] args) {
-		System.out.println("Test");
+		String userIn;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Scanner is initialized.\n-----------------------\nCurrent file directory: " +
+				DIRECTORY + "\nInput 'help' for command list\n");
+		//String test = userIn.nextLine();
+		
+		
+		scan.close();
 	}
 
 }
