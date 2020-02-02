9a837d57a95df9d5b0428d5dafc56ff9c7254692
Ian Higa <ian.higa@sbcglobal.net>
2020-02-02 14:42:40 -0800

    CLEANUP
     - Got rid of all of the help functions and such. THe program really
    just needs to house the scan function...

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -11,79 +11,56 @@ public class MainApplication {
 	public static void main(String[] args) {
 		String userIn = null;
 		Scanner scan = new Scanner(System.in);
+		String in = "";
+		String out = "";
+		boolean io = false;
 		
-		System.out.println("Scanner is initialized.\n-----------------------\nCurrent file directory: " +
-				DIRECTORY + "\nInput 'quit' to quit\nInput 'help' for command list\n");
-		do {
-			System.out.println("Awaiting next command...\n");
-			userIn = scan.nextLine();
-			if(userIn.contentEquals("help")) {
-				File input = new File(DIRECTORY + "commands" + FILE_EXTENSION);
-				Scanner tempScan = null;
-				try {
-					tempScan = new Scanner(input);
-				} catch (FileNotFoundException e) {
-					System.out.println("No command documentation found. Terminating 'help' command...\n");
-					e.printStackTrace();
+		userIn = scan.nextLine();
+		char[] command = userIn.toCharArray();
+				
+		for(int i = 5; i < command.length; i++) {
+			if(command[i] != ' ' && !io) {
+				in += command[i];
+			}
+			else if (io) {
+				if(command[i] == ' ') {
 					break;
 				}
-				while(tempScan.hasNext()) {
-					System.out.println(tempScan.nextLine());
-				}
-				tempScan.close();
+				out += command[i];
 			}
-			else if(userIn.contains("scan")) {
-				String in = "";
-				String out = "";
-				boolean io = false;
-				char[] command = userIn.toCharArray();
+			else {
+				io = true;
+			}	
+		}
 				
-				for(int i = 5; i < command.length; i++) {
-					if(command[i] != ' ' && !io) {
-						in += command[i];
-					}
-					else if (io) {
-						if(command[i] == ' ') {
-							break;
-						}
-						out += command[i];
-					}
-					else {
-						io = true;
-					}	
-				}
+		if(in.contentEquals("") || out.contentEquals("")) {
+			System.out.println("An error has occured:\nNot enough arguments.\n");
+		}
+		else if(in.contentEquals("commands") || out.contentEquals("commands")){
+			System.out.println("An error has occured:\nCannot use commands file as input or output.\n");
+		}
+		else {
+			File input = new File(DIRECTORY + in + FILE_EXTENSION);
+			File output = new File(DIRECTORY + out + FILE_EXTENSION);
+
+			if(input.exists() && ! (output.exists())){		
+				Scanner inScan = new Scanner(DIRECTORY + in + FILE_EXTENSION);
 				
-				if(in.contentEquals("") || out.contentEquals("")) {
-					System.out.println("An error has occured:\nNot enough arguments.\n");
+			}
+			else {
+				System.out.println("An error has occured:\n");
+				if(! input.exists()) {
+					System.out.println("Input file " + DIRECTORY + in + FILE_EXTENSION + " does not exist.\n");
 				}
-				else if(in.contentEquals("commands") || out.contentEquals("commands")){
-					System.out.println("An error has occured:\nCannot use commands file as input or output.\n");
-				}
-				else {
-					File input = new File(DIRECTORY + in + FILE_EXTENSION);
-					File output = new File(DIRECTORY + out + FILE_EXTENSION);
-					
-					if(input.exists() && ! (output.exists())){					
-						scanInputFile(in, out);
-					}
-					else {
-						System.out.println("An error has occured:\n");
-						if(! input.exists()) {
-							System.out.println("Input file " + DIRECTORY + in + FILE_EXTENSION + " does not exist.\n");
-						}
-						if(output.exists()) {
-							System.out.println("Output file " + DIRECTORY + out + FILE_EXTENSION + " already exists.\n");
-						}
-					}
+				if(output.exists()) {
+					System.out.println("Output file " + DIRECTORY + out + FILE_EXTENSION + " already exists.\n");
 				}
 			}
-		} while(! (userIn.contentEquals("quit")));
-		
-		System.out.println("\nShutting down...");
+		}
 		scan.close();
 	}
 	
-	private static void scanInputFile(String in, String out) {
+	private static void scanInputLine(String in) {
 		
 	}
 
